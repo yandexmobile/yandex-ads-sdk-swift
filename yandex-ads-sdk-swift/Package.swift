59fa061a9c02d2a3d722c7ef871bead852e57795
mobile-ads-github <mobile-ads-github@yandex-team.ru>
2023-05-02 11:19:50 +0200

    5.6.0

---
+++ yandex-ads-sdk-swift/Package.swift
@@ -0,0 +1,61 @@
+// swift-tools-version: 5.7; 
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "YandexMobileAdsPackage",
+    platforms: [
+        .iOS("12.0")
+    ],
+    products: [
+        .library(
+            name: "YandexMobileAdsPackage",
+            targets: ["YandexMobileAdsTarget"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/divkit/divkit-ios", "23.0.0"..<"24.0.0"),
+        .package(url: "https://github.com/yandexmobile/metrica-sdk-ios", "4.0.0"..<"5.0.0")
+    ],
+    targets: [
+        .target(
+            name: "YandexMobileAdsTarget",
+            dependencies: [
+                .target(name: "YandexMobileAds"),
+                .target(name: "YandexMobileAdsInstream"),
+                .product(name: "DivKit", package: "divkit-ios"),
+                .product(name: "YandexMobileMetrica", package: "metrica-sdk-ios")
+            ],
+            path: "YandexMobileAdsTarget",
+            linkerSettings: [
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("AdSupport"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("CoreImage"),
+                .linkedFramework("CoreLocation"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("CoreTelephony"),
+                .linkedFramework("CoreText"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("Network"),
+                .linkedFramework("QuartzCore"),
+                .linkedFramework("StoreKit"),
+                .linkedFramework("SwiftUI"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("UIKit"),
+                .linkedFramework("WebKit")
+            ]
+        ),
+        .binaryTarget(
+            name: "YandexMobileAds",
+            url: "https://github.com/yandexmobile/yandex-ads-sdk-swift/releases/download/5.6.0/YandexMobileAds.xcframework.zip",
+            checksum: "a9fc4922871bebef92c6afb2f3f194796fb6acf5217a23cb115db5bb3d15e82f"
+        ),
+        .binaryTarget(
+            name: "YandexMobileAdsInstream",
+            url: "https://github.com/yandexmobile/yandex-ads-sdk-swift/releases/download/5.6.0/YandexMobileAdsInstream.xcframework.zip",
+            checksum: "1aa461b165e0edea5915bfbee3476ed2d110d5791412c50429660dcffbac05f0"
+        )
+    ]
+)
