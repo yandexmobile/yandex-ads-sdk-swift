e673102a4500f74ffda052094850b62c1bf25436
mobile-ads-github <31896236+mobile-ads-github@users.noreply.github.com>
2023-08-28 16:00:20 +0300

    Update Package.swift due to release 6.0.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "YandexMobileAdsPackage",
-            targets: ["MobileAdsTarget"]
+            targets: ["YandexMobileAdsTarget"]
         )
     ],
     dependencies: [
